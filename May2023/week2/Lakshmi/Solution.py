a12967e4c87de0b828145edb9afd5c9308241594
lakshmi pathakamuri <80885515+703265LAKSHMI@users.noreply.github.com>
2023-05-12 19:26:08 +0530

    Update Solution.py

--- May2023/week2/Lakshmi/Solution.py
+++ May2023/week2/Lakshmi/Solution.py
@@ -1,1 +1,21 @@
 
+a, b = map(float, input().split())
+c, d = map(float, input().split())
+
+
+x = complex(a, b)
+y = complex(c, d)
+sum = x + y
+diff = x - y
+prod = x * y
+div = x / y
+mod_x = abs(x)
+mod_y = abs(y)
+
+print('{:.2f}{:+.2f}i'.format(sum.real, sum.imag))
+print('{:.2f}{:+.2f}i'.format(diff.real, diff.imag))
+print('{:.2f}{:+.2f}i'.format(prod.real, prod.imag))
+print('{:.2f}{:+.2f}i'.format(div.real, div.imag))
+print('{:.2f}{:+.2f}i'.format(mod_x.real, mod_x.imag))
+print('{:.2f}{:+.2f}i'.format(mod_y.real, mod_y.imag))
+
