7cccca9b4d3b564ff7dcf83d829e561c1111c893
lakshmi pathakamuri <80885515+703265LAKSHMI@users.noreply.github.com>
2023-05-16 11:59:12 +0530

    Create Solution.py

---
+++ May2023/Week3/Lakshmi/Solution.py
@@ -0,0 +1,14 @@
+from __future__ import division
+
+
+a = int(input())
+b = int(input())
+
+
+integer_division = a // b
+modulus = a % b
+divmod_result = divmod(a, b)
+
+print(integer_division)
+print(modulus)
+print(divmod_result)
